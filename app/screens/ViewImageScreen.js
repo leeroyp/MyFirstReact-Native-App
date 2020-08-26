356e7a2f918911370578a6832634b63051b3c5ce
Leeroy Phili <leeroyphili@gmail.com>
2020-08-26 15:46:07 -0400

    updates

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 
+import colors from '../config/colors'
+
 export default function ViewImageScreen() {
     return (
 <View style={styles.container}>
@@ -20,19 +22,19 @@ const styles = StyleSheet.create({
     closeIcon: {
         width: 50,
         height: 50,
-        backgroundColor: '#fc5c65',
+        backgroundColor: colors.primary,
         position: "absolute",
         top:40,
         left:30,
     },
     container:{
-        backgroundColor: "#000",
+        backgroundColor: colors.black,
         flex: 1
     },
     deleteIcon: {
         width: 50,
         height: 50,
-        backgroundColor: '#4ecdc4',
+        backgroundColor: colors.secondary,
         position: "absolute",
         top:40,
         right:30,
